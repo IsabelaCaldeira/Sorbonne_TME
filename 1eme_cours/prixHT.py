393458408d6d6df1508e79f4b4031ddfcd9c60e2
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-09-18 20:28:43 +0200

    adding first exercice

---
+++ 1eme_cours/prixHT.py
@@ -0,0 +1,10 @@
+def prixHT(tc: float, tva: float) -> float :
+    """_summary_
+
+    Args:
+        tc (float): _description_
+        tva (float): _description_
+
+    Returns:
+        float: _description_
+    """
