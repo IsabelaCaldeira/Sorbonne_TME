aae3f035405a22ef53b8c0d70ce3b26bbb9c1635
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-09-20 12:51:22 +0200

    add exercice 1.6 q1

---
+++ 1eme_cours/nombreFermat.py
@@ -0,0 +1,12 @@
+#Exercice 1.6 Question 1
+def nombre_fermat(n : float) -> float:
+    """
+    Préconditions n => 0
+    Retourne calculer le nombre de Fermat
+    """
+    return (2**2**n)+1
+
+assert nombre_fermat(0) == 3
+assert nombre_fermat(0) == 3
+
+#Question 2
