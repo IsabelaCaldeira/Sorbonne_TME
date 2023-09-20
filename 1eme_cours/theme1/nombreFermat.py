4f019069e715f7acf1c225a2632de5dc5c930718
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-09-20 16:56:57 +0200

    organizing theme1

--- 1eme_cours/theme1/nombreFermat.py
+++ 1eme_cours/theme1/nombreFermat.py
@@ -7,6 +7,12 @@ def nombre_fermat(n : float) -> float:
     return (2**2**n)+1
 
 assert nombre_fermat(0) == 3
-assert nombre_fermat(0) == 3
+assert nombre_fermat(1) == 5
+assert nombre_fermat(2) == 17
 
 #Question 2
+def nombre_premier(n:float)-> str:
+    """Pŕeconditions n => 0
+    Vérifier si Fn est premier ou non-premier.
+    """
+   
