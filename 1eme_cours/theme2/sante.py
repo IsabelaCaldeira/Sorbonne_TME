85ef01b74a56a16273c39ffc63a6741a43f3c2bc
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-09-22 15:04:19 +0200

    finish exercice 2 question 2

---
+++ 1eme_cours/theme2/sante.py
@@ -0,0 +1,14 @@
+#Exercice 2 Question 1
+def sante(x : float) -> str:
+    """Retourne Bonne santé si x vaut 37.5, retourne "Malade" sinon.
+    """
+    if x == 37.5: 
+        return "Bonne santé"
+    else:
+        return "Malade"
+    
+assert sante(38) == "Malade"
+assert sante(37.5) == "Bonne santé"
+
+"Le code vérifie si la condition de x est vrai et retourne des responses pour chaque condition"
+
