e7194159ae3e1bdf09c68ae9bdf52e992b45c583
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-09-22 15:55:38 +0200

    adding Exercice 2.3 Questoin2

--- 1eme_cours/theme2/couverture.py
+++ 1eme_cours/theme2/couverture.py
@@ -22,3 +22,29 @@ assert f(6, 18, 4) == 'cas 5'
 assert f(9, 17, 15) == 'cas 2'
 assert f(1.3, 0.7, 3.7) == 'cas 3'
 assert f(9, 9, 9) == 'cas 6'
+
+#Exercice 2.3 Question 2 
+
+def f_autre(n1 : float, n2 : float, n3 : float) -> str:
+    """Précondition : n1 != n2 and n2 != n3 and n3 != n1
+    retourne un cas parmi 6 selon les valeurs de n1, n2 et n3.
+    """
+    if n1 < n2 < n3:
+        return 'cas 1'
+    elif n1 < n3 < n2:
+        return 'cas 2'
+    elif n2 < n1 < n3:
+        return 'cas 3'
+    elif n2 < n3 < n1:
+        return 'cas 4'
+    elif n3 < n1 < n2:
+        return 'cas 5'
+    else:
+        return 'cas 6'
+    
+assert f_autre(2.3, 2.5, 2.7) == 'cas 1'
+assert f_autre(7, 2, 5) == 'cas 4'
+assert f_autre(6, 18, 4) == 'cas 5'
+assert f_autre(9, 17, 15) == 'cas 2'
+assert f_autre(1.3, 0.7, 3.7) == 'cas 3'
+assert f_autre(9, 9, 9) == 'cas 6'
