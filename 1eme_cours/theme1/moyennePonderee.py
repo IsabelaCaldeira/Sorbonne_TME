a18264ac4344e92921218bee9f66538172114382
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-09-24 23:56:09 +0200

    correcting exercice 1.1

--- 1eme_cours/theme1/moyennePonderee.py
+++ 1eme_cours/theme1/moyennePonderee.py
@@ -6,6 +6,7 @@ def moyenne_ponderee(a: float, b: float, c: float, pa: float, pb: float, pc:floa
     """
     return (a*pa + b*pb + c*pc)/( pa + pb + pc )
 
+print(moyenne_ponderee(5, 10, 40, 1, 2,1))
 assert moyenne_ponderee(2,2,2,2,2,2) == 2
-assert moyenne_ponderee(1,2,3,3,2,2) == 13/7
-assert moyenne_ponderee(5, 10, 40, 1, 2, 1) == 68
+assert moyenne_ponderee(1,2,3,3,3,2) == 1.875
+assert moyenne_ponderee(5, 10, 40, 1, 2, 1) == 16.25
