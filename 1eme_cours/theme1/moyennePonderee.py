089fd38176a9fea92b09a2e1a2aa9340e503eb40
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-09-24 23:56:32 +0200

    fixing error

--- 1eme_cours/theme1/moyennePonderee.py
+++ 1eme_cours/theme1/moyennePonderee.py
@@ -6,7 +6,6 @@ def moyenne_ponderee(a: float, b: float, c: float, pa: float, pb: float, pc:floa
     """
     return (a*pa + b*pb + c*pc)/( pa + pb + pc )
 
-print(moyenne_ponderee(5, 10, 40, 1, 2,1))
 assert moyenne_ponderee(2,2,2,2,2,2) == 2
 assert moyenne_ponderee(1,2,3,3,3,2) == 1.875
 assert moyenne_ponderee(5, 10, 40, 1, 2, 1) == 16.25
