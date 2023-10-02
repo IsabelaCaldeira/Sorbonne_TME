0f21a6d3089a039f12db075e3da265490d1c3afe
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-10-02 09:03:56 +0200

    exercice 3.5

--- theme3/2eme_cours/fibonnaci.py
+++ theme3/2eme_cours/fibonnaci.py
@@ -1,3 +1,4 @@
+import math 
 #Exercice 3.5 Question 1
 def fibonacci(n:int)->int:
     """Précondition n>=0
@@ -9,10 +10,11 @@ def fibonacci(n:int)->int:
         return 1
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
-        
-assert fibonacci(8) == 21
 
-#Exercice 3.5 Question 2
+assert fibonacci(3) == 2
+     
+#Exercice 3.5 Question 2   
+assert fibonacci(8) == 21
 
 #Exercice3.5 Question 3
 def fibo_diff(n: int) -> float:
@@ -20,3 +22,16 @@ def fibo_diff(n: int) -> float:
     Renvoie le k-ième de le terme n"""
 
     return fibonacci(n) // fibonacci(n-1)
+
+assert fibo_diff(3) == 2
+assert fibo_diff(2) == 1
+
+#Exercice 3.5 Question 4
+def fibo_approx(n: int) -> float:
+    """Précondition n >= 5
+    Renvoie le approximation de fibonacci 
+    """
+    
+    return (((1 + math.sqrt(5)) / 2)**n)/math.sqrt(5)
+
+assert 4 < fibo_approx(5) < 5
