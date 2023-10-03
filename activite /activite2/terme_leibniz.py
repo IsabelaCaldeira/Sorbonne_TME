a22dd2d0b5beef9bfc811a5f90bf5e28a216f976
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-10-03 22:09:03 +0200

    Exercice 4 Activite

--- activite /activite2/terme_leibniz.py
+++ activite /activite2/terme_leibniz.py
@@ -1,3 +1,4 @@
+import random
 import math 
 #Exercice 1 Question 1 
 def terme_leibniz(n : int) -> float:
@@ -42,3 +43,20 @@ assert approx_leibniz(100) == 3.1514934010709914
 assert abs(approx_leibniz(10)-3.232315) < 10**6 
 assert abs(approx_leibniz(100)-3.1514934) < 10**7
 
+#Exercice 4 Suggestion Aléatoire
+def approx_aleatoire(N: int) -> float:
+    """Précondition:
+    Calcule une approximation de pi
+    """
+    a : int = 0
+    i : int
+    for i in range(N+1):
+        x : float = random.random()
+        y : float = random.random()
+        if math.sqrt(((1/2) - x) ** 2 + ((1/2)-y)**2)<= (1/2):
+            a = a + 1
+
+    return (a/N)*4
+    
+assert 2.5 < approx_aleatoire(50) < 3.5
+assert 2.5 < approx_aleatoire(1000) < 3.5
