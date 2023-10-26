a75ff72527a99f9125e7d06eb5ca92c2a1343cdd
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-10-26 11:15:49 +0200

    updating theme4

--- theme4/2eme_cours/somme_cubes.py
+++ theme4/2eme_cours/somme_cubes.py
@@ -1,14 +1,29 @@
+#Exercice 4.4 Question 1
 def somme_cubes(n : int) -> int:
     """
-        Précondition
-        retourne ???
+        Précondition n >=0 
+        Renvoie la somme de cubes
     """
-    # ?? à quoi sert s ?
+    # La somme c'est la variable que va prends le valeur de la somme de cubes 
+    
     somme : int = 0
     k : int = 1
-    while k <= n:
-        somme = somme + (n ** 3)
+    
+    while k <= n :
+        somme = somme + (k ** 3)
         k = k + 1
+        
     return somme
 
-print(somme_cubes(1))
+assert somme_cubes(0) == 0
+assert somme_cubes(1) == 1
+assert somme_cubes(2) == 9 
+assert somme_cubes(3) == 36
+assert somme_cubes(4) == 100
+
+#Exercice 4.4 Question 2 
+assert somme_cubes(4) == 0+1**3+2**3+3**3+4**3 
+
+
+
+
