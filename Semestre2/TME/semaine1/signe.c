c5e21e7d0f14f0d42e6c7ad8a93258073d83782e
Isabela Caldeira <isabela.ribeiro@outlook.com>
2024-02-01 16:52:34 +0100

    adding signe

---
+++ Semestre2/TME/semaine1/signe.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <assert.h>
+
+int signeProduit(int m, int n){
+    if ((m == 0) || (n == 0)){
+        return 0;
+    } else {
+        if ((m < 0) || (n < 0)){
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
+
+int main(){
+    assert(signeProduit(3,3) == 1);
+    assert(signeProduit(3,-2) == -1);
+    assert(signeProduit(3,0) == 0);
+    return 0;
+}
