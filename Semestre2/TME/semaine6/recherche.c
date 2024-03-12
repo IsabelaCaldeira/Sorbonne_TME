c8dbe7cd18b7db95d1a6260104f82dcc1b449076
Isabela Caldeira <isabela.ribeiro@outlook.com>
2024-03-12 12:02:25 +0100

    Adding recherche_rec

--- Semestre2/TME/semaine6/recherche.c
+++ Semestre2/TME/semaine6/recherche.c
@@ -16,37 +16,38 @@ int recherche_iter(int *tab, int taille, int elem){
 
 
 int recherche_rec(int *tab, int taille, int elem){
+    printf(" taille %d et tab[taille -1] %d\n", taille, tab[taille-1]);
     if(tab[taille - 1] == elem) return taille - 1;
     else{
         if(taille == 0) return -1;
-        else recherche_rec(tab, taille -1, elem);
+        else return recherche_rec(tab, taille -1, elem);
     }
 }
 
 int main() {
    {
     int tab[]={1,1,1,1,1,1,1};
-printf("%d\n",recherche_iter(tab,7,1));;
+printf("%d\n",recherche_rec(tab,7,1));;
    }
     printf("%s\n", SEPARATOR);   {
     int tab[]={1,1,1,1,1,1,1};
-printf("%d\n",recherche_iter(tab,7,5));;
+printf("%d\n",recherche_rec(tab,7,5));;
    }
     printf("%s\n", SEPARATOR);   {
     int tab[]={1,2,3,4,5,6,7,8,9,10};
-printf("%d\n",recherche_iter(tab,10,10));;
+printf("%d\n",recherche_rec(tab,10,1));;
    }
     printf("%s\n", SEPARATOR);   {
     int tab[]={10,9,8,7,6,5,4,3,2,1};
-printf("%d\n",recherche_iter(tab,10,6));;
+printf("%d\n",recherche_rec(tab,10,6));;
    }
     printf("%s\n", SEPARATOR);   {
     int tab[]={1,2,1,2,1,2,1,2,1,2,1,2};
-printf("%d\n",recherche_iter(tab,12,2));;
+printf("%d\n",recherche_rec(tab,12,2));;
    }
     printf("%s\n", SEPARATOR);   {
     int tab[]={10,2,9,3,8,1,7,4,6,5};
-printf("%d\n",recherche_iter(tab,10,5));;
+printf("%d\n",recherche_rec(tab,10,5));;
    }
         return 0;
 }
