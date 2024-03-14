9f203222d6f520abfb7aa91afba42245ad2358b6
Isabela Caldeira <isabela.ribeiro@outlook.com>
2024-03-14 10:58:07 +0100

    adding signe

--- Semestre2/TME/semaine1/signe.c
+++ Semestre2/TME/semaine1/signe.c
@@ -5,6 +5,7 @@ int signeProduit(int m, int n){
     if ((m == 0) || (n == 0)){
         return 0;
     } else {
+        if ((m < 0) && (n < 0)) return 1;
         if ((m < 0) || (n < 0)){
             return -1;
         } else {
@@ -15,7 +16,7 @@ int signeProduit(int m, int n){
 
 int main(){
     assert(signeProduit(3,3) == 1);
-    assert(signeProduit(3,-2) == -1);
+    assert(signeProduit(-1,-2) == 1);
     assert(signeProduit(3,0) == 0);
     return 0;
 }
