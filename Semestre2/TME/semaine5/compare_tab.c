a397c1a046a6c96f5160ac6e73e0628070325bb0
Isabela Caldeira <isabela.ribeiro@outlook.com>
2024-03-14 17:47:03 +0100

    udpating compare_tab

--- Semestre2/TME/semaine5/compare_tab.c
+++ Semestre2/TME/semaine5/compare_tab.c
@@ -10,24 +10,20 @@ int compare_tab(int *tab1, int *tab2){
     int i = 0;
     
     while (tab1[i] != -1 && tab2[i] != -1) {
-        if (tab1[i] != tab2[i]) {
-            return -1;
-        }
+        if (tab1[i] != tab2[i]) return -1;
         i++;
     }
     
-    if (tab1[i] == -1 && tab2[i] == -1) {
-        return 0; 
-    } else {
-        return -1; 
-    }
+    if (tab1[i] == -1 && tab2[i] == -1) return 0; 
+    else return -1; 
+    
 }
 
 int main() {
    {
     int tab1[]={1,1,5,4,7,-1};
-int tab2[]={1,1,5,4,7,-1};
-printf("%d\n",compare_tab(tab1,tab2));;
+    int tab2[]={1,1,5,4,7,-1};
+    printf("%d\n",compare_tab(tab1,tab2));;
    }
     printf("%s\n", SEPARATOR);   {
     int tab1[]={1,1,5,-1};
