a8c5bc6bce636e91c5e3c20200434e5eeb409167
Isabela Caldeira <isabela.ribeiro@outlook.com>
2024-03-15 11:07:13 +0100

    finish versoin of est_deb

--- Semestre2/TME/semaine6/est_deb.c
+++ Semestre2/TME/semaine6/est_deb.c
@@ -13,21 +13,35 @@ int est_deb(char *chaine1, char *chaine2){
     return est_deb(chaine1 + 1, chaine2 + 1);
 }
 
+int est_incluse(char *chaine1, char *chaine2){
+    if(*chaine1 == '\0') return 1;
+    if(*chaine2 == '\0') return 0;
+    if(*chaine1 != *chaine2) return est_incluse(chaine1, chaine2 + 1);
+
+    return est_incluse(chaine1 + 1, chaine2 + 1);
+}
+
 int main() {
    {
-    printf("%d\n",est_deb("alpha","alphabet"));;
+    printf("%d\n",est_incluse("alpha","alphabet"));;
    }
     printf("%s\n", SEPARATOR);   {
-    printf("%d\n",est_deb("alpaga","alphabet"));;
+    printf("%d\n",est_incluse("alpaga","alphabet"));;
    }
     printf("%s\n", SEPARATOR);   {
-    printf("%d\n",est_deb("aaaaaaaaa","aaaaaaaaa"));;
+    printf("%d\n",est_incluse("abe","alphabet"));;
    }
     printf("%s\n", SEPARATOR);   {
-    printf("%d\n",est_deb("aaaaaaaaa","aaaaaaaab"));;
+    printf("%d\n",est_incluse("beta","alphabet"));;
    }
     printf("%s\n", SEPARATOR);   {
-    printf("%d\n",est_deb("","aaaaaaaaa"));;
+    printf("%d\n",est_incluse("","alphabet"));;
+   }
+    printf("%s\n", SEPARATOR);   {
+    printf("%d\n",est_incluse("bet","alphabet"));;
+   }
+    printf("%s\n", SEPARATOR);   {
+    printf("%d\n",est_incluse("ber","alphabet"));;
    }
         return 0;
 }
