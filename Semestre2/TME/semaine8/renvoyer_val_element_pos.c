c8611a1dc4d3267c0e5da3de0a32b75f057d9d03
Isabela Caldeira <isabela.ribeiro@outlook.com>
2024-03-27 18:24:32 +0100

    finding x value in liste

---
+++ Semestre2/TME/semaine8/renvoyer_val_element_pos.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+typedef struct _cellule_t cellule_t;
+
+struct _cellule_t {
+  int donnee;
+  cellule_t *suivant;
+};
+
+int renvoyer_val_element_pos(int pos, cellule_t* liste){
+    cellule_t* current = liste;
+    int i = 0;
+    
+    while (current != NULL) {
+        if (i == pos) {
+            return current->donnee;
+        }
+        current = current->suivant;
+        i++;
+    }
+    
+    return -1;
+}
+
+int main() {
+   int i, n;
+   scanf("%d", &n);
+   cellule_t *ma_liste = creerListe(n);
+   scanf("%d", &i);
+   assert (i >= 0 && i < n);
+   printf("%d\n", renvoyer_val_element_pos(i, ma_liste));
+   return 0;
+}
