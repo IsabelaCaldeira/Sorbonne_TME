1d7a64bb13a2c4e5584b17d5ea790f84d98d49fd
Isabela Caldeira <isabela.ribeiro@outlook.com>
2024-04-01 13:54:54 +0200

    adding min max moy

--- Semestre2/TME/semaine3/min_max_moy.c
+++ Semestre2/TME/semaine3/min_max_moy.c
@@ -1,72 +1,102 @@
 #include <stdio.h>
 
-#define MIN(a,b) ((a)<(b) ? (a):(b))
-#define MAX(a,b) ((a)>(b) ? (a):(b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 #define VAL1 -2
 #define VAL2 7
 #define VAL3 5
 #define VAL4 9
 
-
-void min_max(int val, int *min, int *max) {
-    printf("Ici c'est le val max et min:%d, %d, %d\n", val, *min, *max);
-    if(*min>val) *min = val;
-    if(*max<val) *max = val;
-}
-
-void stats (int v1, int v2, int v3, int v4, int *min, int *max, float *moy){
-  
-  if(v1<=0) *max = *min = *moy = -1;
-  else{
-    float count = 0.0;
-    float somme = 0.0;
-
-    min_max(v1, min, max);
-    somme += v1;
-    count ++;
-  
-    if(v2>0){
-      min_max(v2, min, max);
-      somme += v2;
-      count ++;  
-
-      if(v3>0){
-        min_max(v3,min, max);
-        somme += v3;
-        count ++;
-      
-        if(v4>0){
-          min_max(v4, min, max);
-          somme += v4;
-          count ++;
-        }
-      }
-    }   
-    *moy = somme/count;
+void min_max(int val, int *min, int *max)
+{
+  if (val < *min)
+  {
+    *min = val;
+  }
+  if (val > *max)
+  {
+    *max = val;
   }
 }
-/*
-void afficher_resultat(float moyenne, int min, int max) {
-  printf("max = %d, min = %d, moy = %.2f\n",max,min,moyenne);
+
+void afficher_min_max(int min, int max)
+{
+  printf("La plus grande des 3 valeurs est %d, la plus petite %d.\n", max, min);
 }
-*/
-int main() {
+
+void stats(int v1, int v2, int v3, int v4, int *min, int *max, float *moy)
+{
+  float count = 0.0;
+  float somme = 0.0;
+
+  if (v1 <= 0)
+  {
+    *min = *max = *moy = -1;
+    return;
+  }
+
+  *min = *max = v1;
+  somme += v1;
+  count++;
+
+  if (v2 > 0)
+  {
+    min_max(v2, min, max);
+    somme += v2;
+    count++;
+  }
+  else
+  {
+    *moy = somme / count;
+    return;
+  }
+
+  if (v3 > 0)
+  {
+    min_max(v3, min, max);
+    somme += v3;
+    count++;
+  }
+  else
+  {
+    *moy = somme / count;
+    return;
+  }
+
+  if (v4 > 0)
+  {
+    min_max(v4, min, max);
+    somme += v4;
+    count++;
+  }
+
+  *moy = somme / count;
+}
+
+void afficher_resultat(float moyenne, int min, int max)
+{
+  printf("max = %d, min = %d, moy = %.2f\n", max, min, moyenne);
+}
+
+int main()
+{
+  int min = 3, max = 8;
+  int a, b, c, d;
   float moy;
-  int min, max;
 
-  /* vous devrez bien sur modifier les parametres d'appel de la fonction stats une fois cette derniere completee */
-  //stats();
-  printf("Entrez deux nombres:\n");
-  scanf("%d", &min);
-  scanf("%d", &max);
-  scanf("%f", &moy);
-  stats(VAL1,VAL2,VAL3,VAL4, &min, &max, &moy);
-  printf("Ici c'est le max: %d\n", max);
-  printf("Ici c'est le min: %d\n", min);
-  printf("Ici c'est le moy: %.2f\n", moy);
+  printf("min = 3, max  = 8. ");
+  printf("Saisissez une valeur entiere :\n");
+  scanf("%d", &a);
 
-  /* vous devrez bien sur modifier les parametres d'appel de la fonction afficher_resultat */
-  //afficher_resultat(0.0,0,0);
+  min_max(a, &min, &max);
+  afficher_min_max(min, max);
 
+  printf("Saisissez trois autres valeurs entieres :\n");
+  scanf("%d", &b);
+  scanf("%d", &c);
+  scanf("%d", &d);
+
+  stats(a, b, c, d, &min, &max, &moy);
+  afficher_resultat(moy, min, max);
   return 0;
 }
