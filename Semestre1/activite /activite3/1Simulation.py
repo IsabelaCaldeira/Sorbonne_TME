664cddb23735dcb4f5212aa78767861219531e79
Isabela Caldeira <isabela.ribeiro@outlook.com>
2024-10-16 14:50:45 +0200

    updating new exercices in activite 3

--- Semestre1/activite /activite3/1Simulation.py
+++ Semestre1/activite /activite3/1Simulation.py
@@ -93,8 +93,51 @@ def est_parfait_invariant(n : int) -> bool :
 
     return n == s
 
-#Question 3 
 
+
+#3 Suggestion Invariant Question 1
+#On utilise l'invariant s avec sa condition: s est égal à la somme de tous les diviseurs de n inférieurs ou égaux à i-1
+#Pour est_divisible(6)
+#Boucle 1, s = 1 == (1)
+#Boucle 2, s = 3 == (2 + 1)
+#Boucle 3, s = 6 == (3 + 2 + 1)
+#Boucle 4, s = 6 == (3 + 2 + 1)
+#Boucle 5, s = 6 == (3 + 2 + 1)
+#Boucle 6, s = 6 == (3 + 2 + 1)
+
+#3 Suggestion Invariant Question 2 
+def invariant(i: int, n:int, s:int) -> bool:
+    """i > 0, n >= 0, s>= 0
+    renvoie si l’invariant est vrai.
+    """
+    new_s : int = 0
+    j : int = 1
+    while j != i:
+        if divise(j, n):
+            new_s = new_s + j
+        j = j + 1
+
+    return s == new_s
+
+assert invariant(6,6,6) == True
+
+#Suggestion 3 Question 3 
+def est_parfait_invariant(n : int) -> bool :
+    """ Pre : n > = 1
+    Decide si n est un nombre parfait ."""
+    i : int = 1
+    s : int = 0
+
+    while i != n :
+        if(invariant(i, n, s)):
+            print("Invariant vrai")
+        if divise(i, n):
+            s = s + i
+        if(invariant(i, n, s)):
+            print("Invariant vrai")
+        i = i + 1
+
+    return n == s
 
 #Suggestion 4 Tableau de Simulation 
 def est_parfait_fichier(n:int) -> bool:
@@ -173,15 +216,24 @@ assert est_parfait_fichier(7) == False
 
 #5 Suggestion : Comparaison de complexité
 #Question 1
-def est_parfait_appels(n:int)->int:
-    """précondition : n>=1
-    affiche le nombre de fois où la fonction divise a été appelée
-    """
-    print(n-1,"appels à la fonction divise dans la fonction est_parfait \n")
-    return (n-1)
+def est_parfait_appels(n : int) -> bool :
+    """ Pre : n >= 1
+    Decide si n est un nombre parfait et affiche le nombre d'appels à divise."""
+    i : int = 1
+    s : int = 0
+    count : int = 0
+
+    while i != n :
+        count += 1
+        if divise(i, n):
+            s = s + i
+        i = i + 1
+
+    print(f"{count} appels à la fonction divise.")
+    return n == s
 
 
-assert est_parfait_appels(100)==99
+assert est_parfait_appels(100) == False
 
 #Question 2
 def est_parfait_opti(n : int) -> bool :
@@ -202,22 +254,43 @@ def est_parfait_opti(n : int) -> bool :
 
 assert est_parfait_opti(100)==False
 
+#Suggestion 5 Question 2
 def est_parfait_opti_appels(n:int)->int:
     """Précondition : n>=1
     compte le nombre d'appel à divise que fait la fonction est_parfait_opti
     """
-    i:int=2
-    a:int=0
+    i: int = 2
+    s: int = 1
+    count: int = 0
     while i != int(math.sqrt(n)) + 1 :
+        count = count+1
         if divise(i, n):
-            a=a+1
+            if i != math.sqrt(n) :
+                s = s + i + (n // i)
+            else :
+                s = s + i
         i=i+1
-    print(a,"appels à la fonction divise dans la fonction est_parfait_opti_appels \n")
-    return a
+    print(count,"appels à la fonction divise dans la fonction est_parfait_opti_appels \n")
+    return count
 
-assert est_parfait_opti_appels(100)==4
-            
+assert est_parfait_opti_appels(100) == 9
+
 #Question 3
+def est_parfait_appels_comparer(n : int) -> int :
+    """ Pre : n >= 1
+    Affiche le nombre d'appels à divise."""
+    i : int = 1
+    s : int = 0
+    count : int = 0
+
+    while i != n :
+        count += 1
+        if divise(i, n):
+            s = s + i
+        i = i + 1
+
+    return count
+
 def comparer(n:int)->bool:
     """précondition : n>=1
     Estime combien d'appels à divise sont faits lors de l'évaluation de est_parfait(n) et de est_parfait_opti(n) en fonction de n.
@@ -226,12 +299,12 @@ def comparer(n:int)->bool:
 
 assert comparer(100)==True
 
-#Question 4
+#Suggestion 5 Question 4
 def image(n:int)->Image:
     """précondition : 1<=n<=200
     produit une image qui représente les courbes du nombre d'appels à divise des deux fonctions (est_parfait(n) et est_parfait_opti(n)), en fonction de n
     """
-    return (overlay(draw_line(-1,-1,n,est_parfait_appels(n)),draw_line(-1,-1,n,est_parfait_opti_appels(n))))
+    return (overlay(draw_line(-1,-1,n,float(est_parfait_appels(n))),draw_line(-1,-1,n,float(est_parfait_appels_comparer(n)))))    
 
 #show_image(image(100))
 
