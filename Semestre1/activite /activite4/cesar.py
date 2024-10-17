8eed1855e8f60d7acecfaad29d2bed45f744cb15
Isabela Caldeira <isabela.ribeiro@outlook.com>
2024-10-17 09:59:31 +0200

    updating new crypto

--- Semestre1/activite /activite4/cesar.py
+++ Semestre1/activite /activite4/cesar.py
@@ -1,3 +1,19 @@
+from typing import List
+
+#Partie 1 
+def identite (s : str) -> str :
+    """ Renvoie la chaine s telle quelle """
+    return s
+
+def identite_texte(nom: str) -> None :
+    """ Precondition : < nom > . txt est un fichier existant
+    recopie le contenu du fichier < nom > . txt dans < nom > − copie .txt """
+    with open(nom + ".txt", "r") as source :
+        with open(nom + " − copie .txt", "w") as destination :
+            ligne : str
+            for ligne in source . readlines () :
+                destination . write ( identite ( ligne ))
+
 #2 Partie Guidée : Chiffre de César
 #Question 1
 def est_minuscule(lettre:str)->bool:
@@ -26,42 +42,30 @@ def caractere_decale(c:str,n:int)->str:
     renvoie le caractère obtenu par un décalage de n
 places dans l'alphabet de c si c est une lettre romaine (majuscule ou minuscule), et c sinon.
     """
-    a : int = ord(c)+n
-    s : int = a-122
-    t : int = a-90
-    u : int = 97 - a
-    v : int = 65 - a
-    
     if est_minuscule(c):
-        if n>=0:
-            if a>122:
-                return chr(96+s)
-            else:
-                return chr(a)
+        a = ord(c) + n
+        if a < ord('a'):
+            return chr(ord('z') - (ord('a') - a - 1))
+        elif a > ord('z'):
+            return chr(ord('a') + (a - ord('z') - 1))
         else:
-            if a<95:
-                return chr(a)
-            else:
-                return chr(96-s)
+            return chr(a)
     elif est_majuscule(c):
-        if n>=0:
-            if a>90:
-                return chr(64+t)
-            else:
-                return chr(a)
+        a = ord(c) + n
+        if a < ord('A'):
+            return chr(ord('Z') - (ord('A') - a - 1))
+        elif a > ord('Z'):
+            return chr(ord('A') + (a - ord('Z') - 1))
         else:
-            if a<90:
-                return chr(a)
-            else:
-                return chr(64-t)
+            return chr(a)
     else:
         return c
-
+    
 assert caractere_decale("a",0)=="a"
 assert caractere_decale("a",3)=="d"
 assert caractere_decale("z",3)=="c"
 
-#Question 3.1
+#Partie Guidee 2 Question 3 
 def ligne_chiffre_cesar(s: str, n: int) -> str:
     """Précondition n>=0 et len(s)>=1
     Renvoie la chaine obtenue en appliquant le chiffrement de César de clef n à s.
@@ -85,7 +89,42 @@ def ligne_dechiffre_cesar(s: str, n: int) -> str:
 assert ligne_dechiffre_cesar (" Erqmrxu OX1LQ011 ", 3) == " Bonjour LU1IN011 "
 assert ligne_dechiffre_cesar (" Bonjour LU1IN011 ", 0) == " Bonjour LU1IN011 "
 
-#4 Suggestion : Chiffre affine
+
+#Partie Guidée Question 4 
+def chiffre_fichier_cesar(nom: str, n: int) -> None:
+    """Precondition: <nom>.txt is an existing file
+    Copies the content of the file <nom>.txt into <nom>-cesar.txt, applying the Caesar cipher with key n to each line.
+    """
+    with open(nom + ".txt", "r") as source:
+        with open(nom + "-cesar.txt", "w") as destination:
+            ligne : str
+            for ligne in source.readlines():
+                destination.write(ligne_chiffre_cesar(ligne, n))
+
+def dechiffre_fichier_cesar(nom: str, n: int) -> None:
+    """Precondition: <nom>-cesar.txt is an existing file
+    Copies the content of the file <nom>-cesar.txt into <nom>-decesar.txt, applying the Caesar cipher with key -n to each line.
+    """
+    with open(nom + "-cesar.txt", "r") as source:
+        with open(nom + "-decesar.txt", "w") as destination:
+            ligne : str
+            for ligne in source.readlines():
+                destination.write(ligne_dechiffre_cesar(ligne, n))  
+
+#3 Suggestion Attaque 
+def attaque_cesar(nom: str) -> None:
+    """Precondition: <nom>.txt est un fichier existant
+    Lit quelques lignes du fichier <nom>.txt, les déchiffre avec chaque clé de 0 à 25, et écrit les résultats dans <nom>-attaque.txt.
+    """
+    with open(nom + ".txt", "r") as source:
+        with open(nom + "-attaque.txt", "w") as destination:
+
+            n : int        
+            for n in range(0,26):
+                for ligne in source.readlines()[:5]:
+                    destination.write(ligne_dechiffre_cesar(ligne,n)+"\n===================\nDécalage de : "+str(n))
+             
+#Suggestion 4 Chiffre Affine
 def pgcd(x:int,y:int)->int:
     """Précondition : x>=y et x>0
     Retourne le plus grand commun diviseur de x et y
@@ -102,6 +141,29 @@ def pgcd(x:int,y:int)->int:
 assert pgcd(9,3)==3
 assert pgcd(15,15)==15
 
+def chiffre_fichier_affine(nom: str, a: int, b: int) -> None:
+    """Pre len(nom) >= 0, a>=0, b>=0
+    Renvoie le chiffre affine de la ligne du fichier nom.txt avec les clefs a et b
+    """
+    x : int = a 
+    while pgcd(x, 26) != 1:
+        x = x + 1
+    with open(nom + ".txt", "r") as source:
+        with open(nom + "-chiffre.txt", "w") as destination:
+            ligne : str
+            for ligne in source.readlines():
+                char : str
+                for char in ligne:
+                    base : int 
+                    if est_majuscule(char):
+                        base = ord('A') 
+                    else:
+                        base = ord('a')
+                    
+                    p : int = ord(char) - base
+                    p_chiffre : int = (x * p + b) % 26
+                    char_chiffre : str = chr(p_chiffre + base)
+                    destination.write(char_chiffre)  
 
 def affine_majuscule(x:int,y:int,p:str)->int:
     """Précondition : len(p)=1
@@ -143,32 +205,57 @@ def ligne_chiffre_affine(nom:str,a:int,b:int)->str:
 
 assert ligne_chiffre_affine("Bonjour LU1IN011!",3,7)=="Kxuixpg OP1FU011!"
 assert ligne_chiffre_affine("Bonjour LU1IN011!",14,47)=="Kxiaxjq EJ1LI011!"
+
 #5 Suggestion: chiffre de vigenère
 def ligne_chiffre_vigenere(l:str, clefs:str)->str:
-    """Fonction qui prend en entrer une chaine de caractères ligne et une chaine de caractère clef et chiffre la ligne selon le chiffrement de vigenere avec la clé clef et renvoie cette ligne chiffré."""
-    ligne_chiffre:str=""
-    sss:str
+    """Fonction qui chiffre la ligne selon le chiffrement de vigenere avec la clé clef."""
+    ligne_chiffre: str = ""
     count:int=0
     ind:List[int]=[]
-    for sss in clefs:
-        if est_majuscule(sss):
-            ind.append(ord(sss)-65)
-        elif est_minuscule(sss):
-            ind.append(ord(sss)-97)
-    lenClef:int=len(clefs)
-    ssss:str
-    for ssss in l:
-        ligne_chiffre=ligne_chiffre+caractere_decale(ssss, ind[count])
-        count=(count+1)%lenClef
+    
+    s : str
+    for s in clefs:
+        if est_majuscule(s):
+            ind.append(ord(s)-65)
+        elif est_minuscule(s):
+            ind.append(ord(s)-97)
+    
+    c : int
+    for c in range(0, len(l)):
+        ligne_chiffre = ligne_chiffre + caractere_decale(l[c], ind[count % len(ind)])
+        count = count + 1
     return ligne_chiffre
+
 assert ligne_chiffre_vigenere("Bonjour", "cle") == "Dzrlzyt"
-assert ligne_chiffre_vigenere("bateau" , "arc") == "brverw" 
+assert ligne_chiffre_vigenere("bateau" , "arc") == "brverw"
 
+def ligne_dechiffre_vigenere(l:str, clefs:str)->str:
+    """Fonction qui déchiffre la ligne selon le chiffrement de vigenere avec la clé clef."""
+    ligne_dechiffre: str = ""
+    count:int=0
+    ind:List[int]=[]
+    
+    s : str
+    for s in clefs:
+        if est_majuscule(s):
+            ind.append(ord(s)-65)
+        elif est_minuscule(s):
+            ind.append(ord(s)-97)
+    
+    c : int
+    for c in range(0, len(l)):
+        ligne_dechiffre = ligne_dechiffre + caractere_decale(l[c], -ind[count % len(ind)])
+        count = count + 1
 
-#suggestion6:
+    return ligne_dechiffre
+
+assert ligne_dechiffre_vigenere("Dzrlzyt", "cle") == "Bonjour"
+assert ligne_dechiffre_vigenere("brverw" , "arc") == "bateau"
+
+#Suggestion 6:
 def ligne_chiffre_scytale (s:str, n:int)->str:
     """précondition:
-renvoie le chiffre avec scytale"""
+    renvoie le chiffre avec scytale"""
     phrase:str=""
     saut:int=0
     colonne: int = len (s) // n 
