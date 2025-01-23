a2f3a645108f2b500e15285a2a6eb76955d1deeb
Isabela Caldeira <isabela.ribeiro@outlook.com>
2025-01-23 17:41:41 +0100

    fixing d == 0

--- Semestre2/TME/semaine1/discriminant.c
+++ Semestre2/TME/semaine1/discriminant.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 #include <math.h> 
 
+/*Question 1*/
 int discriminant(int a, int b, int c){
     return (b*b) -(4*a*c);
 }
 
+/*Question 2*/
 int afficheRacines(int a, int b, int c){
     int d = discriminant(a,b,c);
     if (d < 0){
         printf("Il n'a pas de racine réelle");
     } else {
         if (d == 0){
-            float x = (-b/(2*a));
+            float x = (-b/(2.0*a));
             printf( "Il y a une racine double égale: %f", x);
         } else {
             float x1 = (-b - sqrt(d))/2*a;
@@ -22,6 +24,7 @@ int afficheRacines(int a, int b, int c){
     return 0;
 }
 
+/*Question */
 int main(){
     printf("%c \n", afficheRacines(1,2,-4));
     return 0;
