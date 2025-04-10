9b7636601d9f292cd3a1f823ef84e14f3988c674
IsabelaCaldeira <isabela.ribeiro@outlook.com>
2025-04-10 14:06:35 +0200

    updating to only check the list ens1 and ens2 once and the tri

--- Semestre2/TME/semaine10/unionMe.c
+++ Semestre2/TME/semaine10/unionMe.c
@@ -30,38 +30,6 @@ element_t* creer_multiEnsemble(int n) {
     return ens;
 }
 
-element_t* unionME(element_t* ens1, element_t* ens2) {
-    element_t* current = NULL; 
-
-    while (ens1 != NULL && ens2 != NULL) {
-        if (ens1->valeur < ens2->valeur) {
-            current = ajout_tete_ensemble(current, ens1->valeur, ens1->frequence);
-            ens1 = ens1->suivant;
-        }
-        else if (ens2->valeur < ens1->valeur) {
-            current = ajout_tete_ensemble(current, ens2->valeur, ens2->frequence);
-            ens2 = ens2->suivant;
-        }
-        else {
-            current = ajout_tete_ensemble(current, ens1->valeur, ens1->frequence + ens2->frequence);
-            ens1 = ens1->suivant;
-            ens2 = ens2->suivant;
-        }
-    }
-
-    // Add remaining elements from ens1 or ens2 to current
-    while (ens1 != NULL) {
-        current = ajout_tete_ensemble(current, ens1->valeur, ens1->frequence);
-        ens1 = ens1->suivant;
-    }
-    while (ens2 != NULL) {
-        current = ajout_tete_ensemble(current, ens2->valeur, ens2->frequence);
-        ens2 = ens2->suivant;
-    }
-
-    return current; // return the result of the union operation
-}
-
 void affiche_ensemble(element_t* ens) {
     while (ens != NULL) {
         printf("(%d, %d) ", ens->valeur, ens->frequence);
@@ -69,6 +37,60 @@ void affiche_ensemble(element_t* ens) {
     }
     printf("\n");
 }
+
+element_t* unionME(element_t* ens1, element_t* ens2) {
+    element_t* current = NULL; 
+
+    while(ens1 || ens2){
+        if(!ens1){
+            current = ajout_tete_ensemble(current, ens2 -> valeur, ens2 -> frequence);
+            ens2 = ens2 -> suivant;
+        } else if(!ens2){
+            current = ajout_tete_ensemble(current, ens1 -> valeur, ens1 -> frequence);
+            ens1 = ens1 -> suivant;
+        } else if(ens1 -> valeur > ens2 -> valeur){
+            current = ajout_tete_ensemble(current, ens1 -> valeur, ens1 -> frequence);
+            ens1 = ens1 -> suivant;
+        } else if(ens1 -> valeur < ens2 -> valeur){
+            current = ajout_tete_ensemble(current, ens2 -> valeur, ens2 -> frequence);
+            ens2 = ens2 -> suivant;
+        } else{
+            current = ajout_tete_ensemble(current, ens1 -> valeur,ens1->frequence + ens2->frequence);
+            ens1 = ens1 -> suivant;
+            ens2 = ens2 -> suivant;
+        }
+    }
+    element_t* res = NULL;
+    while(current) {
+        element_t* temp = current;
+        current = current->suivant;
+
+        element_t* existeDeja = res;
+        while (existeDeja && existeDeja->valeur != temp->valeur) {
+            existeDeja = existeDeja->suivant;
+        }
+
+        if (existeDeja) {
+            existeDeja->frequence += temp->frequence;
+            free(temp);
+        } else {
+            if (!res || temp->valeur > res->valeur) {
+                temp->suivant = res;
+                res = temp;
+            } else {
+                element_t* current_sorted = res;
+                while (current_sorted->suivant != NULL && current_sorted->suivant->valeur > temp->valeur) {
+                    current_sorted = current_sorted->suivant;
+                }
+                temp->suivant = current_sorted->suivant;
+                current_sorted->suivant = temp;
+            }
+        }
+    }
+
+    return res;
+}
+
 
 int main()
 {
