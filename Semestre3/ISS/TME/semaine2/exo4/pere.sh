b73e6a99f1a495f937a68eb508970c252c6d24ec
IsabelaCaldeira <isabela.ribeiro@outlook.com>
2025-09-23 05:51:45 +0200

    Adding error message and boucle with shell

--- Semestre3/ISS/TME/semaine2/exo4/pere.sh
+++ Semestre3/ISS/TME/semaine2/exo4/pere.sh
@@ -1,10 +1,11 @@
-
-
-if
+#Il faut toujours fairer gaffe avec les espaces dans les []
+if [ $# -eq 0 ]; then
     echo Il manque un paramètre
-    echo Usage : ./pere.sh <nb_file>
-    
-    echo Je suis $$
-    for i in {1..10} ; do
-        ./fils.sh
-    done
+    echo "Usage : ./pere.sh <nb_file>"
+    exit 1
+fi
+
+echo Je suis $$
+for i in $(seq 1 "$1"); do
+    fils.sh
+done
